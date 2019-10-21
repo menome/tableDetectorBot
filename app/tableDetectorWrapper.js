fa5921e241ba9a21d39e97d80fbbca8bbf70f663
Mbarley <mbarley66@gmail.com>
2019-10-21 14:47:23 -0600

    changed output location

--- app/tableDetectorWrapper.js
+++ app/tableDetectorWrapper.js
@@ -14,21 +14,22 @@ module.exports = function(bot) {
     var filePath= "/tmp/" + msg.Uuid + ".png"
     return helpers.getFile(bot, msg.Library, msg.Path, filePath).then((filePath)=>{
       return new Promise((resolve,reject) => {
-        var command = "tabulo predict " + filePath + " --checkpoint 6aac7a1e8a8e -f /srv/app/output.json -d /srv/app/output"
+        var command = "tabulo predict " + filePath + " --checkpoint 6aac7a1e8a8e -f /tmp/output.json -d /tmp/output"
         bot.logger.info("Running command: " + command)
         execFile(command,{maxBuffer: 1024000,cwd: __dirname+"/.."},(err,stdout,stderr) => {
           if(err) {
             bot.logger.error("subprocess stderr:", stderr + '\n' + err);
             return reject(err);
           }
+          bot.logger.info("tabulo returned. " + stdout)
           //open ../output.json
-          var obj = JSON.parse(fs.readFileSync('/srv/app/output.json', 'utf8'));
+          var obj = JSON.parse(fs.readFileSync('/tmp/output.json', 'utf8'));
           // Parse stdout as JSON. Will throw an error on failure. 
           var response = JSON.parse(obj);
           bot.logger.info(response); 
           // delete the file       
           helpers.deleteFile(filePath); 
-          helpers.deleteFile("/srv/app/output/*"); 
+          helpers.deleteFile("/tmp/output/*"); 
           // return probability
           return resolve(response); 
         })
